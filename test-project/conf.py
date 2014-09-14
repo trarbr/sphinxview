41e1ea8a0a6c05f9a558793de0dbafe713353557
Troels Brødsgaard <trarbr@gmail.com>
2014-09-14 13:03:39 +0200

    modify test-project to use installed version of sphinxview

--- test-project/conf.py
+++ test-project/conf.py
@@ -20,7 +20,6 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('_extensions'))
 
 # -- General configuration ------------------------------------------------
 
@@ -30,7 +29,7 @@ sys.path.append(os.path.abspath('_extensions'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxview']
+extensions = ['sphinxview.ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
