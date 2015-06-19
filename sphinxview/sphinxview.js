dd0a7458efa4376be2fdebfd306110024ce72878
Troels Brødsgaard <tr@arbr.dk>
2015-06-19 13:08:25 +0200

    add semicolon

--- sphinxview/sphinxview.js
+++ sphinxview/sphinxview.js
@@ -2,7 +2,7 @@
 // License: 2-clause BSD, see LICENSE for details
 
 $(document).ready(function() {
-    var url = '/polling?build_file=' + location.pathname
+    var url = '/polling?build_file=' + location.pathname;
     $.ajax(url, {
         type: 'HEAD',
         success: function(response) {
