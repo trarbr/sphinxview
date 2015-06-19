a058f376dccbef9ab066bf48f88d496b8fa66da3
Troels Brødsgaard <tr@arbr.dk>
2015-06-19 12:35:29 +0200

    use jQuery for XHR

--- sphinxview/sphinxview.js
+++ sphinxview/sphinxview.js
@@ -1,20 +1,12 @@
-// Copyright 2014 Troels Brødsgaard
+// Copyright 2015 Troels Brødsgaard
 // License: 2-clause BSD, see LICENSE for details
 
-$(document).ready(function () {
-  var xmlHttp = null;
-  setTimeout(function () {
-    if (window.XMLHttpRequest) {
-      xmlHttp = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-      xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == '200') {
-        window.location.reload(true);
-      }
-    }
-    xmlHttp.open('HEAD', '/polling?build_file=' + location.pathname, true);
-    xmlHttp.send(null);
-  }, 0);
+$(document).ready(function() {
+    var url = '/polling?build_file=' + location.pathname
+    $.ajax(url, {
+        type: 'HEAD',
+        success: function(response) {
+            window.location.reload(true);
+        }
+    });
 });
